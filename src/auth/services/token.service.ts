81da569827c3585e4535f454044b18dec9256981
ProVitSer <prokinvs@gmail.com>
2023-10-24 21:21:26 +0000

    ref: token payload? add token expires

--- src/auth/services/token.service.ts
+++ src/auth/services/token.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as argon2 from 'argon2';
 import { GetTokenResult } from '../interfaces/auth.interface';
 import { JwtPayloadDataAdapter } from '../adapters/jwt-payload-data.adapter';
+import * as moment from 'moment';
 
 @Injectable()
 export class TokenService {
@@ -10,18 +10,14 @@ export class TokenService {
 
     public async getTokens(data: JwtPayloadDataAdapter): Promise<GetTokenResult> {
         return {
-            accessToken: await this.getToken(data, process.env.JWT_SECRET, process.env.JWT_EXPIRATION_TIME),
-            refreshToken: await this.getToken(data, process.env.REFRESH_TOKEN_SECRET, process.env.REFRESH_TOKEN_EXPIRATION_TIME),
+            accessToken: await this.getToken(data.clientId, process.env.JWT_AT_SECRET, process.env.JWT_AT_EXP),
+            refreshToken: await this.getToken(data.clientId, process.env.JWT_RT_SECRET, process.env.JWT_RT_EXP),
+            accessTokenExpires: moment().add(600, 'minutes').unix(),
         };
     }
 
-    public async getToken(data: JwtPayloadDataAdapter, secret: string, expiresIn: string) {
-        delete data.user;
-        return await this.jwtService.signAsync({ ...data }, { secret, expiresIn });
-    }
-
-    public async verifyPassword(plainTextPassword: string, hashedPassword: string): Promise<boolean> {
-        return await argon2.verify(plainTextPassword, hashedPassword);
+    public async getToken(sub: string, secret: string, expiresIn: string) {
+        return await this.jwtService.signAsync({ sub }, { secret, expiresIn });
     }
 
     public async verify(token: string): Promise<any> {
