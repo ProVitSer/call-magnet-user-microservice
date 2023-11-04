79dcbb05637174cc3a7cf27353a1b36a712fcd31
ProVitSer <prokinvs@gmail.com>
2023-11-04 21:49:06 +0000

    ref

--- src/auth/adapters/jwt-payload-data.adapter.ts
+++ src/auth/adapters/jwt-payload-data.adapter.ts
@@ -1,19 +1,23 @@
-import { Role, Status } from '@app/platform-types/user/types';
-import { User } from '@app/users/schemas/users.schema';
+// import { Menu } from '@app/platform-types/user/interfaces';
+// import { Role, Status } from '@app/platform-types/user/types';
+// import { User } from '@app/users/schemas/users.schema';
+// import { STATIC_MENU_BY_ROLE } from '@app/users/users.constants';
 
-export class JwtPayloadDataAdapter {
-    public clientId: string;
-    public firstname: string;
-    public lastname: string;
-    public email: string;
-    public role: Role;
-    public profileStatus: Status;
-    constructor(public user: User) {
-        this.clientId = user.clientId;
-        this.firstname = user.firstname;
-        this.lastname = user.lastname;
-        this.email = user.email;
-        this.role = user.role;
-        this.profileStatus = user.status;
-    }
-}
+// export class JwtPayloadDataAdapter {
+//     public clientId: string;
+//     public firstname: string;
+//     public lastname: string;
+//     public email: string;
+//     public role: Role;
+//     public profileStatus: Status;
+//     public menu: Menu[];
+//     constructor(public user: User) {
+//         this.clientId = user.clientId;
+//         this.firstname = user.firstname;
+//         this.lastname = user.lastname;
+//         this.email = user.email;
+//         this.role = user.role;
+//         this.profileStatus = user.status;
+//         this.menu = STATIC_MENU_BY_ROLE[user.role];
+//     }
+// }
