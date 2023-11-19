c5a49513261d1193e0cd38ebb7400746a28aacab
ProVitSer <prokinvs@gmail.com>
2023-11-19 20:36:45 +0000

    feat: add company

--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -22,6 +22,9 @@ export class User extends Document {
     @Prop({ required: true })
     password: string;
 
+    @Prop({ required: true })
+    company: string;
+
     @Prop({ type: String, unique: true })
     clientId: string;
 
