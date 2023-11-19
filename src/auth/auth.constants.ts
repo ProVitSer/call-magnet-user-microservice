d6077b56f8fb221cbf3545f9b48ecbc7a91eb05a
ProVitSer <prokinvs@gmail.com>
2023-11-19 20:37:23 +0000

    ref: user create success description

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,4 +1,4 @@
-export const USER_CREATE_SUCCESS = 'Пользователь успешно создан';
+export const USER_CREATE_SUCCESS = 'Пользователь успешно создан. Письмо для подтверждения отправлено на почту';
 export const USRR_ACTIVATE_SUCCESS = 'Пользователь успешно прошел верификацию';
 export const PASSWORD_UPDATE_SUCCESS = 'Пароль пользователя успешно изменен';
 export const CONFIRM_UPDATE_PASSWORD = 'Письмо для сброса пароля успешно отправлено';
